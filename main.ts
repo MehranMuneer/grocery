165e104eec5ebafe9dbb072865aa34e00a9ee604
MehranMuneer <120187244+MehranMuneer@users.noreply.github.com>
2022-12-21 21:23:10 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,43 +1,33 @@
-basic.showLeds(`
-    # # . # #
-    . # # # .
-    . . # . .
-    . . # . .
-    . . # . .
-    `)
-basic.showString(" Grocery Store List")
-let text_list = [
-"a",
+input.onButtonPressed(Button.A, function () {
+    list += -1
+    basic.showString("" + (text_list[list]))
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + (text_list[list]))
+})
+input.onButtonPressed(Button.B, function () {
+    list += 1
+    basic.showString("" + (text_list[list]))
+})
+let text_list: string[] = []
+let list = 0
+list = 0
+text_list = [
+"Bread",
 "b",
 "c",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-""
+"d",
+"e",
+"f",
+"g",
+"h",
+"i",
+"j",
+"k",
+"l",
+"m",
+"n",
+"o",
+"p"
 ]
+basic.showString("" + (text_list.length))
